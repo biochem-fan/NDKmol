0e310aac6838d4907995d4933705c000866a40b0
biochem-fan <biochem_fan@users.sourceforge.jp>
2013-12-19 22:43:03 +0000

    Updated license notice in imported files

--- Classes/MolecularView.mm
+++ Classes/MolecularView.mm
@@ -1,10 +1,21 @@
-//
-//  EAGLView.m
-//  MolecularViewer
-//
-//  Created by Takanori Nakane on 12/01/13.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
-//
+/*  NDKmol - Molecular Viewer on Android NDK and iOS
+ 
+ (C) Copyright 2011 - 2013, biochem_fan
+ 
+ This file is part of NDKmol.
+ 
+ NDKmol is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #import <QuartzCore/QuartzCore.h>
 
